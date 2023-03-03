556f4d41b12b978ca4bd4019688f64bee1b64ae4
mariakingcilin <mariakingcilin@gmail.com>
2023-03-03 09:33:56 +0530

    add

---
+++ main.js
@@ -0,0 +1,17 @@
+var a=[1,2,3,10,12,13,6,7];
+for (i=0;i<a.length;i++)
+{
+	document.write(a[i]);
+	document.write("<br>");
+}
+
+
+var a=[[1,2,3,4],[5,6,7,8]];
+for(i=0;i<a.length;i++)
+{
+	for(j=0;j<a[i].length;j++)
+	{
+		document.write(a[i][j]);
+		document.write("<br>");
+	}
+}
